340ac752d82e1ebdb0503b0d0464f2a9fe8587d8
Bobbie Dinh <bobbiedinh888@gmail.com>
2022-03-16 04:07:44 -0700

    added visual of stack

--- recursion.py
+++ recursion.py
@@ -1,16 +1,37 @@
+#example of how recursion works
+def firstMethod():
+    secondMethod()
+    print("first method")
+
+def secondMethod():
+    thirdMethod()
+    print("second method")
+
+def thirdMethod():
+    fourthMethod()
+    print("third method")
+
+def fourthMethod():
+    print("fourth method")
+
+firstMethod()
+
+#what happened in the above code? well firstMethod called the secondMethod, but cannot be completed because secondMethod has not yet been completed.
+#this requires firstMethod to be stored in memory, then proceeds to call secondMethod.
+#Due to this, secondMethod is then placed on top of firstMethod creating the stack
+#it continues until it reaches fourthMethod in which fourthMethod completes itself with the print statement.
+#once fourthMethod is completed it then gets removed from memory(the stack) and continues back to thirdMethod.
+
+
 
 #you have a stack of hundreds and you need to know how much money you have
-
 from random import randrange
 
 def recursion(stack, totalAmount=0):
     if (stack==0):
         return totalAmount
     totalAmount+=100
+    print(totalAmount)
     return recursion(stack-1, totalAmount)
 
 stack = randrange(1, 100)
-print(stack)
-
-print(recursion(stack))
-
