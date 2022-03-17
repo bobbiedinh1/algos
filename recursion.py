26d629f1970db77e141fdfbb9ad031dec77c7c4b
Bobbie Dinh <bobbiedinh888@gmail.com>
2022-03-17 01:23:27 -0700

    added fibonacci sequence

--- recursion.py
+++ recursion.py
@@ -1,22 +1,22 @@
 from random import randrange
 
 #example of how recursion works
-def firstMethod():
-    secondMethod()
-    print("first method")
+# def firstMethod():
+#     secondMethod()
+#     # print("first method")
 
-def secondMethod():
-    thirdMethod()
-    print("second method")
+# def secondMethod():
+#     thirdMethod()
+#     # print("second method")
 
-def thirdMethod():
-    fourthMethod()
-    print("third method")
+# def thirdMethod():
+#     fourthMethod()
+#     # print("third method")
 
-def fourthMethod():
-    print("fourth method")
+# def fourthMethod():
+#     # print("fourth method")
 
-firstMethod()
+# firstMethod()
 
 #what happened in the above code? well firstMethod called the secondMethod, but cannot be completed because secondMethod has not yet been completed.
 #this requires firstMethod to be stored in memory, then proceeds to call secondMethod.
@@ -29,20 +29,37 @@ firstMethod()
 
 #you have a stack of hundreds and you need to know how much money you have
 
-def recursion(stack, totalAmount=0):
-    if (stack==0):
-        return totalAmount
-    totalAmount+=100
-    return recursion(stack-1, totalAmount)
+# def recursion(stack, totalAmount=0):
+#     if (stack==0):
+#         return totalAmount
+#     totalAmount+=100
+#     return recursion(stack-1, totalAmount)
 
-stack = randrange(1, 100)
-print(stack)
-print(recursion(stack))
+# stack = randrange(1, 100)
+# # print(stack)
+# # print(recursion(stack))
 
-def iteration(stack, totalAmount=0):
-    while stack>0:
-        totalAmount+=100
-        stack-=1
-    return totalAmount
+# def iteration(stack, totalAmount=0):
+#     while stack>0:
+#         totalAmount+=100
+#         stack-=1
+#     return totalAmount
 
-print(iteration(stack))
+# # print(iteration(stack))
+
+# def factorial(n):
+#     assert n>=0 and int(n) == n, 'must be greater or = to 0 and positive'
+#     if n in [0,1]:
+#         return 1
+#     return (n * factorial(n-1))
+
+# print(factorial(4))
+
+
+def fibonacci(n):
+    assert n>=0 and int(n) == n, 'must be greater or = to 0 and positive'
+    if n in [0,1]:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(-6))
