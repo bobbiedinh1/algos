fd7b0d3da33f11140c99625f3a68aa758f5ad934
Bobbie Dinh <bobbiedinh888@gmail.com>
2022-03-17 19:38:56 -0700

    added conditions to 3

--- interviewQuestions.py
+++ interviewQuestions.py
@@ -18,6 +18,11 @@ def power(num, exp):
 #3. find gcd of two digits
 
 def gcd(a,b):
+    assert int(a)==a and int(b)==b, "must be integers"
+    if a<0:
+        a=-1*a
+    if b<0:
+        b=-1*a
     if b==0:
         return a
     return (gcd(b, a%b))
