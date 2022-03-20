ca5a983b90f6a1282574afd1ecb6612bb8f98237
Bobbie Dinh <bobbiedinh888@gmail.com>
2022-03-19 20:53:15 -0700

    added in array practice

---
+++ arrayPractice.py
@@ -0,0 +1,7 @@
+#1. create array
+#2. access indivdual elements through indexes
+#3. append any value to the array using append() 
+#4. insert value in array using insert()
+#5. extend array using extend()
+#6. add items from list into array using fromList()
+#7.
