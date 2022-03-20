6ccb75a8d2909b1c1c9adbf8fdd201aacac8fb09
Bobbie Dinh <bobbiedinh888@gmail.com>
2022-03-19 20:58:48 -0700

    added more practice problems

--- arrayPractice.py
+++ arrayPractice.py
@@ -4,4 +4,13 @@
 #4. insert value in array using insert()
 #5. extend array using extend()
 #6. add items from list into array using fromList()
-#7.
+#7. remove any array element using remove()
+#8. remove last array element using pop()
+#9. fetch any element through its index using index()
+#10. reverse a python array using reverse()
+#11. get array buffer information through buffer_info()
+#12. check for number of occurrences of an element using count()
+#13. convert array to string using tostring()
+#14. convert array to a python list with same elements using tolist()
+#15. append a string to char array using fromstring()
+#16. slice elements from an array
