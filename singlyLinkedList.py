d9fab4680852a55d554447c15a75a824374dcff6
Bobbie Dinh <bobbiedinh888@gmail.com>
2022-03-23 18:06:17 -0700

    created node and singly list

---
+++ singlyLinkedList.py
@@ -0,0 +1,14 @@
+#create the node
+
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+#create the singly list
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
