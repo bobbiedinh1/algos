fc9f57037ccacb07876a21c631ac452e81593fab
Bobbie Dinh <bobbiedinh888@gmail.com>
2022-03-23 18:17:16 -0700

    add to beginning function

--- singlyLinkedList.py
+++ singlyLinkedList.py
@@ -12,3 +12,15 @@ class SinglyLinkedList:
         self.head = None
         self.tail = None
 
+#create def for adding to beginning of list
+
+def addToBeginning(node, sList):
+    #if list is empty then the node becomes the head
+    if (sList.head==None):
+        sList.head = node
+        sList.tail = node
+    #else the new node.next will follow the current heads address then take its place as the head node
+    else:
+        node.next = sList.head
+        sList.head = node
+
